d217b8feed4b00785c9574804e4ae73ac8b20ad3
jhaugh42 <jhaugh42@gmail.com>
2015-09-01 05:32:11 -0400

    a test to verify travis setup

---
+++ test/sample-test.js
@@ -0,0 +1,8 @@
+
+describe('a sample test', function() {
+    var assert = require('assert');
+   it('should pass because it does nothing', function(){
+      assert.ok('it is okay');
+   });
+
+});
