b011bea17b9375b0b3f9c5f1c1a121bc229696a1
jhaugh42 <jhaugh42@gmail.com>
2015-09-01 18:08:56 -0400

    attempt at coveralls via travis

--- test/sample-test.js
+++
@@ -1,8 +0,0 @@
-
-describe('a sample test', function() {
-    var assert = require('assert');
-   it('should pass because it does nothing', function(){
-      assert.ok('it is okay');
-   });
-
-});
